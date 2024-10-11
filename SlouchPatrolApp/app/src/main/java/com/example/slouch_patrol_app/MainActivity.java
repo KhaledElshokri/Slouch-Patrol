d5b61bf403f5677b0e87898b071c4af9946cd1ba
kylerasinger <kyle.rasinger@gmail.com>
2024-10-11 12:58:27 -0400

    Added home page, and routing for settings and data page

--- SlouchPatrolApp/app/src/main/java/com/example/slouch_patrol_app/MainActivity.java
+++ SlouchPatrolApp/app/src/main/java/com/example/slouch_patrol_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.slouch_patrol_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+        Main activity is parent activity to data and settings activities.
+
+        For the collection of data, i think we're best off having a polling function to the sensors,
+        which then updates the database, inserts into the algorithm and changes all displays.
+        This way we can MAYBE avoid having to have complicated back and forth requests.
+        Not sure really, - Kyle
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //settings button set up
+        Button buttonSettings = findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(v -> routeToSettings());
+
+        Button buttonData = findViewById(R.id.buttonData);
+        buttonData.setOnClickListener(v -> routeToData());
+    }
+
+    private void routeToSettings(){
+        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    private void routeToData(){
+        Intent intent = new Intent(MainActivity.this, DataActivity.class);
+        startActivity(intent);
     }
 }
---
+++ SlouchPatrolApp/app/src/main/java/com/example/slouch_patrol_app/SettingsActivity.java
@@ -0,0 +1,47 @@
+package com.example.slouch_patrol_app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_settings);
+
+        //set up toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    //allow toolbar to route back to parent activity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
