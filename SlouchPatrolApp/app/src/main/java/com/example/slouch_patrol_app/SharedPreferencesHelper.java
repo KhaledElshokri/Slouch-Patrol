03840e609fe112f7b790bd02f38f9ec9e980be49
TimGitsGood <76269740+TimGitsGood@users.noreply.github.com>
2024-10-28 11:54:42 -0400

    Create SharedPrefHelper Class
    Create Class used to manipulate saved data in sharedpreferences.

---
+++ SlouchPatrolApp/app/src/main/java/com/example/slouch_patrol_app/SharedPreferencesHelper.java
@@ -0,0 +1,40 @@
+package com.example.slouch_patrol_app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesHelper {
+
+    private SharedPreferences sharedPreferences;
+    private DeviceSettings deviceSettings;
+
+    public SharedPreferencesHelper(Context context) {
+        deviceSettings = new DeviceSettings();
+
+        sharedPreferences = context.getSharedPreferences("DevicePreferences", Context.MODE_PRIVATE);
+        deviceSettings.setDeviceName(sharedPreferences.getString("DeviceName", null));
+        deviceSettings.setLastInitialized(sharedPreferences.getString("LastInitialized", ""));
+        deviceSettings.setPushNotifications(sharedPreferences.getBoolean("PushNotifications", false));
+        deviceSettings.setConnectionStatus(sharedPreferences.getString("ConnectionStatus", ""));
+    }
+
+    public DeviceSettings getDeviceSettings() {
+        return deviceSettings;
+    }
+
+    public void setDeviceSettings(DeviceSettings deviceSettings) {
+        this.deviceSettings = deviceSettings;
+    }
+
+    public void saveDeviceSettings(DeviceSettings deviceSettings) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("DeviceName", deviceSettings.getDeviceName());
+        editor.putString("LastInitialized", deviceSettings.getLastInitialized());
+        editor.putBoolean("PushNotifications", deviceSettings.isPushNotifications());
+        editor.putString("ConnectionStatus", deviceSettings.getConnectionStatus());
+        editor.apply();
+    }
+
+}
+
+
