7d62ddf5ee523dcf51cc43e6ccad121a1197e265
TimGitsGood <76269740+TimGitsGood@users.noreply.github.com>
2024-11-03 13:18:51 -0500

    Fixed Class Conflicts
    Class conflicts with testing library

--- SlouchPatrolApp/app/src/main/java/com/example/slouch_patrol_app/UnitTests/MailgunUnitTests.java
+++ SlouchPatrolApp/app/src/main/java/com/example/slouch_patrol_app/UnitTests/MailgunUnitTests.java
@@ -3,22 +3,13 @@ package com.example.slouch_patrol_app.UnitTests;
 import static com.example.slouch_patrol_app.Mailgun.sendProdEmail;
 import static com.example.slouch_patrol_app.Mailgun.sendNonProdEmail;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import com.example.slouch_patrol_app.BuildConfig;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class MailgunUnitTests {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
 
     // TEST PROD EMAILS WITH MAILGUN
     @Test
